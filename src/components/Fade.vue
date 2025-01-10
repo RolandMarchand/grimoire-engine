091535fe0e869599e4e81da6b7ac13aba11db53e
Roland Marchand <roland.marchand@protonmail.com>
2025-01-10 15:33:43 -0500

    Fixed the behavior of the fade component

--- src/components/Fade.vue
+++ src/components/Fade.vue
@@ -1,45 +1,25 @@
 <script setup lang="ts">
-function isAnimationEnabled(): boolean {
-    // TODO: implement
-    return true;
-}
+
 </script>
 
 <template>
-    <div id="fade-div">
-        <!-- Animations on -->
-        <template v-if="isAnimationEnabled()">
-            <TransitionGroup appear>
-                <slot></slot>
-            </TransitionGroup>
-        </template>
-
-        <!-- Animations off -->
-        <template v-else>
-            <slot></slot>
-        </template>
-    </div>
+    <TransitionGroup appear name="fade" tag="div" id="fade-div">
+        <slot></slot>
+    </TransitionGroup>
 </template>
 
 <style>
 #fade-div > * {
     opacity: 0.6;
+    transition: opacity 1s ease;
 }
 
-#fade-div > *:last-child {
+#fade-div > :last-child {
     opacity: 1;
 }
 
-.v-enter-active {
-    transition: opacity 1s ease-out;
-}
-
-.v-leave-active {
-    transition: opacity 1s ease-in;
-}
-
-#fade-div > .v-enter-from,
-#fade-div > .v-leave-to {
+#fade-div > .fade-enter-from,
+#fade-div > .fade-leave-to {
     opacity: 0;
 }
 </style>
